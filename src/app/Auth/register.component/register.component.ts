05b5af555ae9607b95bef6576613968c5de308f7
username / Alessandro Mandolito <https://github.com/alexmando/ingw-es1.git>
2025-07-13 21:17:40 +0200

    ancora

--- src/app/Auth/register.component/register.component.ts
+++ src/app/Auth/register.component/register.component.ts
@@ -33,7 +33,7 @@ export class RegisterComponent implements OnInit {
 
   ngOnInit() {
     // Test di connessione al backend
-    this.http.get('http://localhost:8080/').subscribe({
+    this.http.get('http://localhost:8080/auth/').subscribe({
       next: (res) => {
         console.log('Backend raggiungibile', res);
       },
